504261e15301cf98e0faf2837fd1103da1d3ea6b
Keith Miller <36780654+Keimille@users.noreply.github.com>
2020-02-11 16:43:29 -0500

    Update variables.tf

--- TerraformReferenceStack/variables.tf
+++ TerraformReferenceStack/variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
 }
 
 variable "vpc_cidr" {
-    description = CIDR for the VPC
+    description = "CIDR for the VPC"
     default = "10.0.0.0/16"
 }
 
@@ -16,6 +16,7 @@ variable "public_subnet_cidr" {
 variable "private_subnet_cidr" {
     description = "CIDR for the public subnet"
     default = "10.0.2.0/24"
+}
 
 variable "ami" {
     description = "Amazon Linux"
